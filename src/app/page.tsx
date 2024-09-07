aa8cc02d07cef60f3622213af76496b7fa5d6cb0
Abdullah Arab <abdo.arab2014@gmail.com>
2024-09-07 13:32:50 +0200

    chore: Update about and home components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { Navbar } from "./components/navbar";
-import { Home } from "./components/home";
-import { About } from "./components/about";
-import { Skills } from "./components/skills";
-import { Projects } from "./components/projects";
-import { Experience } from "./components/experience";
-import { Contact } from "./components/contact";
-
+import { Navbar } from "../components/navbar";
+import { Home } from "../components/home";
+import { About } from "../components/about";
+import { Skills } from "../components/skills";
+import { Projects } from "../components/projects";
+import { Experience } from "../components/experience";
+import { Contact } from "../components/contact";
 function Page() {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,20 +1,16 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import { Button } from "./ui/button";
+import Link from 'next/link'
+import { Button } from "@/components/ui/button"
 
 export function Home() {
   return (
-    <section
-      id="home"
-      className="min-h-screen flex flex-col justify-center items-center text-center p-6 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
-    >
+    <section id="home" className="min-h-screen flex flex-col justify-center items-center text-center p-6 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
       <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
         Hi, I'm Abdullah Arab – turning ideas into apps that matter.
       </h1>
       <p className="text-xl md:text-2xl text-white mb-8">
-        Specialized in Flutter and React development, with a knack for creating
-        intuitive user experiences and scalable software solutions.
+        Specialized in Flutter and React development, with a knack for creating intuitive user experiences and scalable software solutions.
       </p>
       <div className="space-x-4">
         <Button asChild>
@@ -25,5 +21,5 @@ export function Home() {
         </Button>
       </div>
     </section>
-  );
+  )
 }
